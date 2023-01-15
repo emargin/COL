e03b3c32a0412197139d81c0c19cf0efc3488851
Efim <pro100efim@gmail.com>
2023-01-15 20:31:08 +0300

    feat: add country icon

--- src/pages/main.jsx
+++ src/pages/main.jsx
@@ -4,6 +4,7 @@ import PageLayout from '../components/PageLayout'
 import InfoWrapper from '../components/InfoWrapper'
 import InfoCard from '../components/InfoCard'
 import {TabPanel, allyProps} from '../components/tabs'
+import flagImg from '../assets/united-kingdom.png'
 
 const styles = {
   root: {
@@ -28,6 +29,7 @@ const styles = {
 const TABS = [{ label: 'Магазины' }, { label: 'Жилье'}, { label: 'Рестараны'}, { label: 'Транспорт'}]
 
 export default function Main() {
+  console.log('flagImg', flagImg)
 
   const [tab, setTab] = useState(0)
   const handleTabChange = (event, newTab) => {
@@ -37,18 +39,23 @@ export default function Main() {
     <PageLayout>
       <Typography  sx={styles.title} variant="h1">
         <Typography sx={styles.mainText} variant="span">
-          Простой и удобный 
+          Просто и удобно 
         </Typography> <br/>
         сравненивайте цены в разных странах мира
       </Typography>
 
 
-      <InfoWrapper style={{paddingBottom: 0}}>      
-      <Tabs value={tab} onChange={handleTabChange}>
-          {TABS.map((item, index) => (
-              <Tab key={index + item.label} label={item.label} {...allyProps(index)} />
-          ))}
-      </Tabs>
+      <InfoWrapper style={{paddingBottom: 0}}> 
+      <Box sx={{display: 'flex', flexDirection: 'row', alignItems: 'center'}}>
+        <img alt="country flag img" src={flagImg.src} style={{width: '24px', height: '24px'}}/>
+
+        <Typography variant="h6">Великобретания</Typography>     
+      </Box>
+        <Tabs value={tab} onChange={handleTabChange}>
+            {TABS.map((item, index) => (
+                <Tab key={index + item.label} label={item.label} {...allyProps(index)} />
+            ))}
+        </Tabs>
       </InfoWrapper>
       <TabPanel value={tab} index={0}>
         <Box sx={{display: 'flex', flexDirection: 'row', gap: '25px'}}>
