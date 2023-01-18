b16374300a432d78ea770e700e397b672c4284e7
Efim <pro100efim@gmail.com>
2023-01-18 06:23:56 +0300

    chore: add theme obj

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,12 @@
 import '@/styles/globals.css'
 
+import { ThemeProvider } from '@mui/material'
+import { theme } from '@/theme'
+
 export default function App({ Component, pageProps }) {
-    return <Component {...pageProps} />
+    return (
+        <ThemeProvider theme={theme}>
+            <Component {...pageProps} />
+        </ThemeProvider>
+    )
 }
---
+++ src/theme.js
@@ -0,0 +1,4 @@
+// import { createTheme } from '@mui/material'
+import { createTheme } from '@mui/material'
+
+export const theme = createTheme()
