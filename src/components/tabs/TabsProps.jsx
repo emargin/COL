eac185aa9f3f86796e5844dffac81a98451bfa0e
Efim <pro100efim@gmail.com>
2023-01-28 16:43:16 +0300

    chote: remove jsx files

--- src/components/tabs/TabsProps.jsx
+++
@@ -1,7 +0,0 @@
-/* eslint-disable import/prefer-default-export */
-export default function allyProps(index) {
-    return {
-        id: `simple-tab-${index}`,
-        'aria-controls': `simple-tabpanel-${index}`,
-    }
-}
--- src/components/tabs/index.jsx
+++
@@ -1,5 +0,0 @@
-import TabContent from './TabConten'
-import TabPanel from './TabPanel'
-import allyProps from './TabsProps'
-
-export { TabContent, TabPanel, allyProps }
