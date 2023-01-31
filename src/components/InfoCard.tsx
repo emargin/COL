c4ad462766adb550e165343a16e26efa6afceb7d
Efim <pro100efim@gmail.com>
2023-01-31 18:43:09 +0300

    styles: add percentage color

--- src/components/InfoCard.tsx
+++ src/components/InfoCard.tsx
@@ -6,18 +6,19 @@ import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown'
 import InfoWrapper from './InfoWrapper'
 import { PricePosition } from './countries/GeneralInfo'
 
+interface InfoCardProps {
+    title: string
+    price: string
+    pricePosition: any
+}
+
 const styles = {
     root: {
         maxWidth: '100%',
         flexGrow: 1,
         flexBasis: 0,
     },
-}
-
-interface InfoCardProps {
-    title: string
-    price: string
-    pricePosition: any
+    subTitle: { display: 'flex', flexDirection: 'row', alignItems: 'center', justifyContent: 'cetner' },
 }
 
 const userCounry = 'Малайзии'
@@ -28,35 +29,20 @@ export default function InfoCard({ title, price, pricePosition }: InfoCardProps)
             {/* TODO: get color from theme */}
             <Typography sx={{ color: '#adb5bd', fontSize: '16px' }}>{title}</Typography>
             <Typography sx={{ fontSize: '24px' }}>{price}</Typography>
-            {/* <Box
-                sx={{ display: 'flex', flexDirection: 'row', alignItems: 'center', justifyContent: 'cetner' }}
-            >
+            <Box sx={styles.subTitle}>
                 <Typography variant="body2">На</Typography>
 
                 {pricePosition.position === PricePosition.LOWER ? (
-                    <Typography variant="body2" sx={{ color: 'red', mt: 'auto', mb: 'auto' }}>
-                        <ArrowDropDownIcon /> {pricePosition.percent}
+                    <Typography variant="body2" sx={{ color: 'red', display: 'flex', mr: 0.5 }}>
+                        <ArrowDropDownIcon fontSize="small" /> {pricePosition.percent}
                     </Typography>
                 ) : (
-                    <Typography variant="body2" sx={{ color: 'green' }}>
-                        <ArrowDropUpIcon /> {pricePosition.percent}{' '}
+                    <Typography variant="body2" sx={{ color: 'green', display: 'flex', mr: 0.5 }}>
+                        <ArrowDropUpIcon fontSize="small" /> {pricePosition.percent}
                     </Typography>
                 )}
                 <Typography variant="body2">{`чем в ${userCounry}`}</Typography>
-            </Box> */}
-            <Typography variant="body2" sx={{ display: 'flex', alignItems: 'center' }}>
-                На
-                {pricePosition.position === PricePosition.LOWER ? (
-                    <>
-                        <ArrowDropDownIcon sx={{ color: 'red' }} /> {pricePosition.percent}
-                    </>
-                ) : (
-                    <>
-                        <ArrowDropUpIcon sx={{ color: 'green' }} /> {pricePosition.percent}
-                    </>
-                )}
-                {` чем в ${userCounry}`}
-            </Typography>
+            </Box>
         </InfoWrapper>
     )
 }
