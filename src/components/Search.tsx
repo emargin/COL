61b054fbf23654b16a01f501bbf58920309d1a40
Efim <pro100efim@gmail.com>
2023-02-19 17:05:52 +0300

    feat: expanded search component

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -1,14 +1,25 @@
 import React from 'react'
 import Link from 'next/link'
-import { Box, Autocomplete, TextField, Typography } from '@mui/material'
+import { Box, Autocomplete, TextField, Typography, Button, IconButton, useMediaQuery } from '@mui/material'
 import { contries } from '@/mock'
 import { useRouter } from 'next/navigation'
+import SwapHorizIcon from '@mui/icons-material/SwapHoriz'
 
 const styles = {
     root: { width: '78%', margin: 'calc(50vh - 140px) auto auto auto' },
     title: {
         textAlign: 'left',
-        mb: 2,
+        mb: 3,
+    },
+    searchWrapper: {
+        bgcolor: 'background.paper',
+        borderRadius: 2,
+        display: 'flex',
+        flexDirection: 'row',
+        alignItems: 'center',
+        '@media screen and (max-width:426px)': {
+            flexWrap: 'wrap',
+        },
     },
     input: {
         width: '100%',
@@ -18,7 +29,7 @@ const styles = {
         },
         '.MuiOutlinedInput-notchedOutline': {
             borderWidth: '2px',
-            borderColor: 'primary.main',
+            borderColor: 'transparent',
         },
         '&:hover:not($disabled) fieldset': {
             borderColor: 'primary.main',
@@ -28,6 +39,7 @@ const styles = {
 }
 
 export default function Search() {
+    const isMobileDevice = useMediaQuery('(max-width:426px)')
     const router = useRouter()
     const [filter, setFilter] = React.useState('')
     const avalibleCountries = React.useMemo(
@@ -42,24 +54,44 @@ export default function Search() {
     return (
         <Box sx={styles.root}>
             <Typography variant="h5" component="h1" sx={styles.title}>
-                Сравнивайте цены в разных странах мира
+                Сравнивайте цены по всему миру
             </Typography>
-            <Autocomplete
-                autoSelect
-                disablePortal
-                freeSolo
-                sx={styles.input}
-                options={avalibleCountries}
-                onInputChange={(_, value) => setFilter(value)}
-                onChange={redirectToCountryInfo}
-                getOptionLabel={(option: any) => (option.name ? option.name : option)}
-                onKeyDown={(e) => e.key === 'Enter' && console.log('submit')}
-                renderInput={(params) => <TextField autoFocus placeholder="Найти страну" {...params} />}
-            />
+            <Box sx={styles.searchWrapper}>
+                <Autocomplete
+                    autoSelect
+                    disablePortal
+                    freeSolo
+                    sx={styles.input}
+                    options={avalibleCountries}
+                    onInputChange={(_, value) => setFilter(value)}
+                    onChange={redirectToCountryInfo}
+                    getOptionLabel={(option: any) => (option.name ? option.name : option)}
+                    onKeyDown={(e) => e.key === 'Enter' && console.log('submit')}
+                    renderInput={(params) => <TextField autoFocus placeholder="Откуда" {...params} />}
+                />
+                <IconButton sx={{ height: '35px', width: '35px' }}>
+                    <SwapHorizIcon
+                        fontSize="small"
+                        sx={isMobileDevice ? { transform: 'rotate(90deg)' } : undefined}
+                    />
+                </IconButton>
+                <Autocomplete
+                    autoSelect
+                    disablePortal
+                    freeSolo
+                    sx={styles.input}
+                    options={avalibleCountries}
+                    onInputChange={(_, value) => setFilter(value)}
+                    onChange={redirectToCountryInfo}
+                    getOptionLabel={(option: any) => (option.name ? option.name : option)}
+                    onKeyDown={(e) => e.key === 'Enter' && console.log('submit')}
+                    renderInput={(params) => <TextField placeholder="Куда" {...params} />}
+                />
+            </Box>
 
-            <Typography variant="body2" sx={styles.info}>
+            {/* <Typography variant="body2" sx={styles.info}>
                 <Link href="/countries/1">Куала-Лумпур</Link>
-            </Typography>
+            </Typography> */}
         </Box>
     )
 }
