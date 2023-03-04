ab4f61824a5930e8610e31c3292d522c54b16097
Efim <pro100efim@gmail.com>
2023-03-04 15:15:33 +0300

    chore: create two info card variants

--- src/components/InfoCard.tsx
+++ src/components/InfoCard.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Box, Typography } from '@mui/material'
+import { Box, Typography, useMediaQuery } from '@mui/material'
 import ArrowDropUpIcon from '@mui/icons-material/ArrowDropUp' // TODO: FIND SVG ANR REMOVE MUI/ICONS LIB
 import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown'
 
@@ -19,45 +19,58 @@ const styles = {
         flexGrow: 1,
         flexBasis: 0,
     },
+
     subTitle: { display: 'flex', flexDirection: 'row', alignItems: 'center', justifyContent: 'cetner' },
+    priceWrapper: { display: 'flex', alignItems: 'center' },
+    price: { fontSize: 24 },
 }
 
 const userCounry = 'Малайзии'
 
-export default function InfoCard({ title, price, pricePosition }: InfoCardProps) {
-    return (
-        <InfoWrapper sx={styles.root}>
-            {/* TODO: get color from theme */}
+const MobileCard = ({ title, price, pricePosition }: InfoCardProps) => (
+    <InfoWrapper sx={styles.root}>
+        <Typography variant="body2" sx={{ color: 'text.secondary' }}>
+            {title}
+        </Typography>
+        <Box sx={styles.priceWrapper}>
+            <Typography sx={styles.price}>{price}</Typography>
+            {pricePosition.position === PricePosition.LOWER ? (
+                <Typography variant="body2" sx={{ color: 'error.main', display: 'flex', mr: 0.5 }}>
+                    <ArrowDropDownIcon fontSize="small" /> {pricePosition.percent}
+                </Typography>
+            ) : (
+                <Typography variant="body2" sx={{ color: 'success.main', display: 'flex', mr: 0.5 }}>
+                    <ArrowDropUpIcon fontSize="small" /> {pricePosition.percent}
+                </Typography>
+            )}
+        </Box>
+    </InfoWrapper>
+)
 
-            <Typography variant="body2" sx={{ color: 'text.secondary' }}>
-                {title}
-            </Typography>
-            <Box sx={{ display: 'flex', alignItems: 'center' }}>
-                <Typography sx={{ fontSize: '24px' }}>{price}</Typography>
-                {pricePosition.position === PricePosition.LOWER ? (
-                    <Typography variant="body2" sx={{ color: 'error.main', display: 'flex', mr: 0.5 }}>
-                        <ArrowDropDownIcon fontSize="small" /> {pricePosition.percent}
-                    </Typography>
-                ) : (
-                    <Typography variant="body2" sx={{ color: 'success.main', display: 'flex', mr: 0.5 }}>
-                        <ArrowDropUpIcon fontSize="small" /> {pricePosition.percent}
-                    </Typography>
-                )}
-            </Box>
-            <Box sx={styles.subTitle}>
-                {/* <Typography variant="body2">На</Typography>
+const DesktopCard = ({ title, price, pricePosition }: InfoCardProps) => (
+    <InfoWrapper sx={styles.root}>
+        <Typography variant="body2" sx={{ color: 'text.secondary' }}>
+            {title}
+        </Typography>
+        <Typography sx={styles.price}>{price}</Typography>
+        <Box sx={styles.subTitle}>
+            <Typography variant="body2">На</Typography>
 
-                {pricePosition.position === PricePosition.LOWER ? (
-                    <Typography variant="body2" sx={{ color: 'error.main', display: 'flex', mr: 0.5 }}>
-                        <ArrowDropDownIcon fontSize="small" /> {pricePosition.percent}
-                    </Typography>
-                ) : (
-                    <Typography variant="body2" sx={{ color: 'success.main', display: 'flex', mr: 0.5 }}>
-                        <ArrowDropUpIcon fontSize="small" /> {pricePosition.percent}
-                    </Typography>
-                )} */}
-                {/* <Typography variant="body2">{`чем в ${userCounry}`}</Typography> */}
-            </Box>
-        </InfoWrapper>
-    )
+            {pricePosition.position === PricePosition.LOWER ? (
+                <Typography variant="body2" sx={{ color: 'error.main', display: 'flex', mr: 0.5 }}>
+                    <ArrowDropDownIcon fontSize="small" /> {pricePosition.percent}
+                </Typography>
+            ) : (
+                <Typography variant="body2" sx={{ color: 'success.main', display: 'flex', mr: 0.5 }}>
+                    <ArrowDropUpIcon fontSize="small" /> {pricePosition.percent}
+                </Typography>
+            )}
+            <Typography variant="body2">{`чем в ${userCounry}`}</Typography>
+        </Box>
+    </InfoWrapper>
+)
+
+export default function InfoCard(props: InfoCardProps) {
+    const isMobileDevice = useMediaQuery('(max-width:600px)')
+    return isMobileDevice ? <MobileCard {...props} /> : <DesktopCard {...props} />
 }
