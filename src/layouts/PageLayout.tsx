f1db5dc174f54cb3c0068cee595ef4ecd30c1d90
Efim <pro100efim@gmail.com>
2023-07-11 19:04:11 +0300

    refactor: add head for page layout

--- src/layouts/PageLayout.tsx
+++ src/layouts/PageLayout.tsx
@@ -1,6 +1,8 @@
 import React from 'react'
+import Head from 'next/head'
 import { Box } from '@mui/material'
 import RootLayout from './RootLayout'
+import { LocalesMap, useLocale } from '@/shared/utils'
 
 const styles = {
     content: {
@@ -8,11 +10,25 @@ const styles = {
         margin: 'auto',
     },
 }
+const locales: LocalesMap = {
+    ru: {
+        title: 'COL - сравненивайте цены в разных странах мира',
+    },
+    en: {
+        title: 'COL - compare prices around the world',
+    },
+}
 
 export default function PageLayout({ children }: Pick<React.HTMLAttributes<HTMLDivElement>, 'children'>) {
+    const t = useLocale(locales)
     return (
-        <RootLayout>
-            <Box sx={styles.content}>{children}</Box>
-        </RootLayout>
+        <>
+            <Head>
+                <title>{t('title')}</title>
+            </Head>
+            <RootLayout>
+                <Box sx={styles.content}>{children}</Box>
+            </RootLayout>
+        </>
     )
 }
