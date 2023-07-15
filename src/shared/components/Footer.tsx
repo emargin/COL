d3a9350118c95a9a614e7dc3747c867d04173cdb
Efim <pro100efim@gmail.com>
2023-07-15 05:49:45 +0300

    feat: change some footer's styles and add links to resources

--- src/shared/components/Footer.tsx
+++ src/shared/components/Footer.tsx
@@ -3,58 +3,66 @@ import Image from 'next/image'
 import { Box, Link, Typography } from '@mui/material'
 const styles = {
     root: {
+        mt: 'auto',
         bgcolor: 'background.paper',
+        p: 5,
     },
     content: {
         margin: 'auto',
         maxWidth: '1290px',
         display: 'flex',
         flexDirection: 'row',
-        justifyContent: 'end',
-        alignItems: 'flex-start',
-        p: 5,
-        gap: '100px',
+        justifyContent: 'space-between',
     },
     group: {
         textAlign: 'start',
     },
+    groupTitle: {
+        color: 'text.secondary',
+        mb: 1,
+    },
+    link: {
+        textDecoration: 'none',
+    },
 }
+
 export default function Footer() {
     return (
         <Box sx={styles.root}>
             <Box sx={styles.content}>
                 <Image alt="Logo footer" src="" />
                 <Box sx={styles.group}>
-                    <Typography variant="h3" fontSize={14} sx={{ color: 'text.secondary' }}>
+                    <Typography variant="h3" fontSize={14} sx={styles.groupTitle}>
                         Report a problem
                     </Typography>
-                    <Link href="https://github.com/emargin/COL/issues">
+                    <Link sx={styles.link} target="_blank" href="https://github.com/emargin/COL/issues">
                         <Typography variant="body2">Git Hub</Typography>
                     </Link>
                 </Box>
 
                 <Box sx={styles.group}>
-                    <Typography variant="h3" fontSize={14} sx={{ color: 'text.secondary' }}>
+                    <Typography variant="h3" fontSize={14} sx={styles.groupTitle}>
                         Contact with us
                     </Typography>
                     <Box sx={{ display: 'flex', flexDirection: 'column' }}>
-                        <Link>
+                        <Link sx={styles.link} target="_blank" href="mailto:efim.margin@gmail.com">
                             <Typography variant="body2">E-mail</Typography>
                         </Link>
-                        <Link>
+                        <Link sx={styles.link} target="_blank" href="https://t.me/emargin">
                             <Typography variant="body2">Telegram</Typography>
                         </Link>
                     </Box>
                 </Box>
                 <Box sx={styles.group}>
-                    <Typography variant="h3" fontSize={14} sx={{ color: 'text.secondary' }}>
+                    <Typography variant="h3" fontSize={14} sx={styles.groupTitle}>
                         Other
                     </Typography>
                     <Box sx={{ display: 'flex', flexDirection: 'column' }}>
-                        <Link>
+                        {/* add privacy policy correct href */}
+                        <Link sx={styles.link} target="_blank" href="https://github.com/emargin/COL">
                             <Typography variant="body2">Privacy Policy</Typography>
                         </Link>
-                        <Link>
+                        <Link sx={styles.link} target="_blank" href="https://github.com/emargin/COL">
                             <Typography variant="body2">Project on GitHub</Typography>
                         </Link>
                     </Box>
