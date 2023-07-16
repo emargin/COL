0c3b176598f9ddcef315b0eae9657d675a819eba
Efim <pro100efim@gmail.com>
2023-07-16 22:38:58 +0300

    feat: add COL logo

--- src/shared/components/Header.tsx
+++ src/shared/components/Header.tsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import Image from 'next/image'
 import Link from 'next/link'
-import { Box, Typography } from '@mui/material'
+import { Box, Typography, useTheme } from '@mui/material'
 import CurrensySelect from '@/features/CurrensySelect'
 import ThemeSwitchBtn from '@/features/ThemeSwitchBtn'
+import logo from '@/shared/assets/logo.svg'
 
 const styles = {
     root: {
@@ -18,20 +19,47 @@ const styles = {
         alignItems: 'center',
         p: 2,
     },
+    linkContainer: {
+        marginLeft: '8px',
+        display: 'flex',
+        alignItems: 'center',
+        position: 'relative',
+    },
+    rect: {
+        zIndex: -1,
+        position: 'absolute',
+        borderRadius: 0.5,
+    },
 }
 
 export default function Header({ hideCurrent = false }: { hideCurrent?: boolean }) {
+    const theme = useTheme()
+    const isDark = theme.palette.mode === 'dark'
     return (
         <Box sx={styles.root} component="header">
             <Box sx={styles.content}>
-                <Link href="/" style={{ marginLeft: '8px' }}>
-                    {/* <Image src={logo} alt="LOGO" width={120} height={50} /> */}
-                    <Typography fontSize={24} fontWeight={800} sx={{ height: '22px' }}>
-                        COL
-                    </Typography>
-                    <Typography variant="caption" color="primary.main" fontWeight={600}>
-                        cost of living
-                    </Typography>
+                <Link href="/">
+                    <Box sx={styles.linkContainer}>
+                        <Image
+                            alt="logo img"
+                            src={logo}
+                            width={36}
+                            height={36}
+                            style={{
+                                zIndex: -1,
+                                position: 'absolute',
+                                borderRadius: 0.5,
+                            }}
+                        />
+                        <Box sx={{ ml: 3.1 }}>
+                            <Typography fontSize={24} fontWeight={800} sx={{ height: '22px' }}>
+                                COL
+                            </Typography>
+                            <Typography sx={{ ml: '-4px' }} variant="caption" fontWeight={600}>
+                                cost of living
+                            </Typography>
+                        </Box>
+                    </Box>
                 </Link>
                 <Box sx={{ display: 'flex', flexDirection: 'row', alignItems: 'center' }}>
                     <ThemeSwitchBtn />
